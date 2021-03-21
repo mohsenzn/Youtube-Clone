615273a3348398c61fa6e916d1d676040dbdc5d0
mohsen Zergni <mohsen.zergani@gmail.com>
2021-03-21 20:50:59 +0330

    add firebase to app

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+const firebaseConfig = {
+    apiKey: "AIzaSyBGvvLM1NFSe35oet6u2bn2EsEAxQ1ro_c",
+    authDomain: "clone-8515b.firebaseapp.com",
+    projectId: "clone-8515b",
+    storageBucket: "clone-8515b.appspot.com",
+    messagingSenderId: "79995558430",
+    appId: "1:79995558430:web:82ae6b9b824b4973176c0f",
+    measurementId: "G-HFX37T7S56"
+  };
+
+
+firebase.initializeApp(firebaseConfig);
